ad96b60c7f27bf649dafd4260292d27126079ff7
vustinchdev <vustinchdev@gmail.com>
2024-05-20 16:02:29 +0300

    refactor: change message-field props

--- src/components/message-field/message-field.tsx
+++ src/components/message-field/message-field.tsx
@@ -1,20 +1,32 @@
+import { ComponentPropsWithoutRef } from 'react'
+
 import { iconSend } from '@/assets'
 
 import s from './message-field.module.scss'
 
-export const MessageField = () => {
+type Props = {
+  onClick: () => void
+} & ComponentPropsWithoutRef<'textarea'>
+
+export const MessageField = ({ onClick, value, ...rest }: Props) => {
   const classNames = {
     buttonSend: s.buttonSend,
     container: s.container,
     textField: s.textField,
   }
 
+  const handleClick = () => {
+    onClick()
+  }
+
   return (
     <div className={classNames.container}>
-      <textarea className={classNames.textField} />
-      <button className={classNames.buttonSend}>
-        <img alt={'send message'} src={iconSend} style={{ width: '100%' }} />
-      </button>
+      <textarea className={classNames.textField} value={value} {...rest} />
+      {value && (
+        <button className={classNames.buttonSend} onClick={handleClick}>
+          <img alt={'send message'} src={iconSend} style={{ width: '100%' }} />
+        </button>
+      )}
     </div>
   )
 }
